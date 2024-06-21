c3dbc1301b81770014b6a5909758ac3813409404
Jeff Henrichs <jeffhenrichs37@gmail.com>
2024-06-21 13:46:23 -0400

    created a router. Able to render the Root page.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import PetDetailsPage from "./pages/detail";
 import PetDetailsNotFound from "./pages/petDetailsNotFound";
 import Root from "./components/root";
 
-import { RouterProvider } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 
 // create router with JSX Route elements
-const appRouter = "REPLACE ME WITH A ROUTER";
+const appRouter = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Root />} />)
+);
 
 function App() {
   return <RouterProvider router={appRouter} />;
