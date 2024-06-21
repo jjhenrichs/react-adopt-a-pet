dd2885fbf950872843d5ad5e9c42bad4fbb45a4a
Jeff Henrichs <jeffhenrichs37@gmail.com>
2024-06-21 13:38:23 -0400

    import RouterProvider to App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import HomePage from './pages/home';
-import SearchPage from './pages/search';
-import PetDetailsPage from './pages/detail';
-import PetDetailsNotFound from './pages/petDetailsNotFound';
-import Root from './components/root';
+import HomePage from "./pages/home";
+import SearchPage from "./pages/search";
+import PetDetailsPage from "./pages/detail";
+import PetDetailsNotFound from "./pages/petDetailsNotFound";
+import Root from "./components/root";
 
-// Add react-router-dom imports
+import { RouterProvider } from "react-router-dom";
 
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = "REPLACE ME WITH A ROUTER";
 
 function App() {
-  return (
-    // replace below with a Router Provider
-    <p>REPLACE ME</p>
-  );
+  return <RouterProvider router={appRouter} />;
 }
 
 export default App;
