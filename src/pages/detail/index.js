3e0e778d389b3db57a8108ce679b3f46d40ef598
Jeff Henrichs <jeffhenrichs37@gmail.com>
2024-06-22 15:09:34 -0400

    Can navigate to the home page from the PetDetailsNotFound page

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -31,7 +31,6 @@ const PetDetailsPage = () => {
         <h3>Loading...</h3>
       ) : error ? (
         <div>
-          {/* Redirect to /pet-details-not-found if there was an error! */}
           <Navigate
             to="/pet-details-not-found"
             element={<PetDetailsNotFound />}
